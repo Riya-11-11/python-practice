64c640b539d55199605393d9da6fd6965afcaa25
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-01-14 20:34:20 +0530

    Add implementation for multiply function that supports both numbers and strings

---
+++ 04_functions/03_solution.py
@@ -0,0 +1,6 @@
+# Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def multiply(x,y):
+    return x*y
+print(multiply(4,5))
+print(multiply("Hi ",3))
