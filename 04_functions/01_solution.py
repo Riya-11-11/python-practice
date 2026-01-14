1888280d71310f225438b2cc8c7d6d10ec83827d
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-01-14 20:32:30 +0530

    Add implementations for square and addition functions, and update questions for function exercises

---
+++ 04_functions/01_solution.py
@@ -0,0 +1,6 @@
+#  Write a function to calculate and return the square of a number.
+
+def square(num):
+    return num**2
+
+print(square(5))  
---
+++ 04_functions/02_solution.py
@@ -0,0 +1,5 @@
+# Create a function that takes two numbers as parameters and returns their sum.
+
+def add_num(a,b):
+    return a+b
+print(add_num(3,7))
